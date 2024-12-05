66336ba2fee37f111637e844dddb54051f19196f
VAbrishamii <vahide.abri@yahoo.com>
2024-12-05 14:46:27 +0100

    update logo src

--- bidify/src/components/Navbar.jsx
+++ bidify/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   const defaultAvatar =
     "https://images.unsplash.com/photo-1579547945413-497e1b99dac0?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&q=80&h=400&w=400";
 
-  const lightLogo = "/bidify ..png";
+  const lightLogo = "/Bidify ..png";
   // const darkLogo = "/dark mode.png";
 
   useEffect(() => {
